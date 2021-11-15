55b76511de2461a807b126df6cd2f116e2a9fe2f
Crunch Rock <lordpiraterock@outlook.com>
2021-11-15 02:10:10 -0500

    added all the cool features of the submissions requirements such as working out an input field and turning its string into a working variable in your script, saving it to json and loading it

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -12,16 +13,25 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public GameObject GameOverText;
+    public Text NameText;
     
+    public int HighScore;
+    public string HighName;
+    public Text HighScoreName;
     private bool m_Started = false;
     private int m_Points;
     
     private bool m_GameOver = false;
 
+    public string playerName; 
+
     
     // Start is called before the first frame update
     void Start()
-    {
+    {   LoadHighScore();
+        playerName = TitleManager.Instance.username;
+        Debug.Log("The game has started and the users name is " + playerName);
+        NameText.text = playerName;
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
         
@@ -67,10 +77,41 @@ public class MainManager : MonoBehaviour
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
     }
-
+[System.Serializable]
+class High
+{
+    public int newHighScore;
+    public string newHighName;
+}
     public void GameOver()
     {
+        SaveHighScore();
         m_GameOver = true;
         GameOverText.SetActive(true);
+    }
+    public void SaveHighScore()
+    {
+        if(m_Points > HighScore){
+            HighScore = m_Points;
+            High data = new High();
+            data.newHighScore = m_Points;
+            data.newHighName = playerName;
+            string json = JsonUtility.ToJson(data);
+            File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+            Debug.Log("this was written" + json );
+        } 
+    }
+    public void LoadHighScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {   
+            string json = File.ReadAllText(path);
+            High data = JsonUtility.FromJson<High>(json);
+
+            HighScore = data.newHighScore;
+            HighName = data.newHighName;
+            HighScoreName.text = "High Score: "+ HighName + " - " + HighScore;
+        }
     }
 }
--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -2,23 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using System.IO;
+[System.Serializable]
 public class TitleManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{   
+    public static TitleManager Instance;
+     public GameObject usernameInput;
+     public string username;
+     public static int BestScore;
+     public Text BestName;
+     public GameObject enterNamePrompt;
+    
+    private void Awake() 
+    {       
+       if (Instance != null)
+       {
+           Destroy(gameObject);
+           return;
+       }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+       Instance = this;
+       DontDestroyOnLoad(gameObject);
+       LoadUserName();
     }
 
     public void StartGame()
-    {
-        SceneManager.LoadScene(1);
+    {   
+         
+        if(username == null)
+        {
+            enterNamePrompt.SetActive(true);
+        }
+        else
+        {
+            SaveUserName();
+            
+            SceneManager.LoadScene(1);
+
+        }
     }
+
+    class SaveData
+   {
+       public string newHighName;
+       public int newHighScore;
+       public int highScore;
+       public string username;
+    
+   }
+
+    public void SaveUserName()
+   {
+       
+       SaveData data = new SaveData();
+       data.username = username;
+
+
+   }
+
+   public void LoadUserName()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {   
+            Debug.Log("Your save file exists and the name is " + username);
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            
+            username = data.newHighName;
+            BestScore = data.newHighScore;
+
+            BestName.text = username + "'s high score of " + BestScore;
+            
+        }
+    }
+
+    public void ReadUsername(string input) 
+    {
+        username = input;
+        
+        StartGame();
+    }
+
+    
 }
